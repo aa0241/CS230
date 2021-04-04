5d9ec2c35195b8abafb12651083b5efa63d7aa91
test <test@gmail.com>
2021-04-04 14:48:40 -0400

    db

---
+++ cs230.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 04, 2021 at 01:57 PM
+-- Server version: 5.7.33-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs230`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `pid` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `descript` text NOT NULL,
+  `picpath` varchar(80) NOT NULL,
+  `upload_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`pid`, `title`, `descript`, `picpath`, `upload_date`, `rating`) VALUES
+(2, 'a', 'a', '../gallery/6055178e112764.15527677.jpg', '2021-03-19 17:28:46', NULL),
+(3, 'a', 'a', '../gallery/605517999ea3e3.28332818.jpg', '2021-03-19 17:28:57', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `profpic` varchar(50) NOT NULL DEFAULT '../images/default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`pid`, `fname`, `uname`, `profpic`) VALUES
+(1, NULL, NULL, '../Desktop/default.png'),
+(2, 'jxjx', 'xkjxjxjx', '../images/default.jpg'),
+(3, 'adel', 'adel', '../images/default.jpg'),
+(4, 'adel', 'dede', '../images/default.jpg'),
+(5, 'ldkkld', '123', '../images/default.jpg'),
+(6, 'adel', 'adel', '../images/default.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `revid` int(11) NOT NULL COMMENT 'id for each review',
+  `itemid` int(11) NOT NULL COMMENT 'item being reviewed',
+  `uname` varchar(80) NOT NULL COMMENT 'user reviewing item',
+  `title` varchar(60) NOT NULL,
+  `reviewtext` text NOT NULL,
+  `revdate` datetime NOT NULL,
+  `ratingnum` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT 'is there at least one review'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`revid`, `itemid`, `uname`, `title`, `reviewtext`, `revdate`, `ratingnum`, `status`) VALUES
+(1, 7, 'adel', 'd dsvd', '  dvdv', '2021-03-27 16:32:15', 3, 1),
+(2, 7, 'adel', 'fefefe', 'fefefefef', '2021-03-27 16:33:52', 4, 1),
+(3, 7, 'adel', 'efefe', 'fvdvdvd', '2021-03-27 16:34:02', 2, 1),
+(4, 3, 'adel', 'dcdcd', 'cdcdc', '2021-03-27 18:33:23', 3, 1),
+(5, 2, 'adel', 'cdcdcd', 'cdcdcd', '2021-03-27 18:33:32', 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COMMENT='user database for signup and signin';
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `fname`, `lname`, `uname`, `password`, `email`) VALUES
+(1, 'adel', 'ded', 'dede', '$2y$10$RIFfUrj5Zoiqo.611dYwOOAWAe6GV1IcHVMlgzZYoWwrxXeQwvZW2', 'ASDA@a.com'),
+(2, 'ldkkld', 'kldkld', '123', '$2y$10$/SuHqM2MXvA3Raj5JYvpZOKgaqqaJFWpWbSO.WH6WBmx3KTSDNh9a', '123@kkd.com'),
+(3, 'adel', 'adel', 'adel', '$2y$10$36r51m3KoMTSmOTJC07MYut0Jvtl2QTmXGoshcYkOmXeRZQpfzEmW', 'adel@adel.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`revid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `revid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id for each review', AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
