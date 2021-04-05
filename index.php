574e2930b4f050f943a4bb93ac9ec9d5fa39250b
aa0241 <77753344+aa0241@users.noreply.github.com>
2021-04-05 22:32:31 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<body>
+     <link rel="stylesheet" href="css/about.css">
+     <div class="about">
+         <h1>About us</h1>
+         <p>rugoerhfoierfoerferipofn</p>
+         </div> 
+         <div class="row">
+           <div class="column">
+               <div class="card">
+                   <img src="images/1.jpeg">
+                   <div class="container">
+                       <h2>CEO</h2>
+                        <p class="c1">Baby Yoda</p>
+                        <p>Email:BABYYODA@StarWars.com</p>
+                   </div>
+               </div>
+           </div>
+           <div class="column">
+             <div class="card">
+                 <img src="images/2.jpeg">
+                 <div class="container">
+                     <h2>CEO</h2>
+                      <p>Luke Skywalker</p>
+                      <p>Email:Luke@StarWars.com</p>
+                 </div>
+             </div>
+         </div>
+         <div class="column">
+             <div class="card">
+                 <img src="images/3.jpeg">
+                 <div class="container">
+                     <h2>CEO</h2>
+                      <p>Padme</p>
+                      <p>Email:Padme@StarWars.com</p>
+                 </div>
+             </div>
+         </div>
+         <div class="column">
+             <div class="card">
+                 <img src="images/4.jpeg">
+                 <div class="container">
+                     <h2>CEO</h2>
+                      <p>The Skywalkers</p>
+                      <p>Email:Skywalkers@StarWars.com</p>
+                 </div>
+             </div>
+         </div>
+         </div>
+ </body>
